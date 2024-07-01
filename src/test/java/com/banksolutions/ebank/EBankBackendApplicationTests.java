1a6391b020a107929aa52c9c2962cfbdbe2e2a8b
Nabil Sabi <nabilsabi01@gmail.com>
2024-07-01 12:04:25 +0100

    create structure

---
+++ src/test/java/com/banksolutions/ebank/EBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banksolutions.ebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
